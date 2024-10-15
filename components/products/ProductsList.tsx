bc36f72a3bc55d817ff80a3c9b6f096f639dea93
idofarit <idofarit@gmail.com>
2024-10-15 20:24:34 +0530

    prp

--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -38,7 +38,7 @@ function ProductsList({ products }: { products: Product[] }) {
               </Card>
             </Link>
             <div className="absolute bottom-8 right-8 z-5">
-              <FavouriteToggleButton productId={productId} />
+              <FavouriteToggleButton />
             </div>
           </article>
         );
