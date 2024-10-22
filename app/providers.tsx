011291f21b126064b0cab05a617ef6c24324dca7
idofarit <idofarit@gmail.com>
2024-10-22 21:43:43 +0530

    auth init & ui setup

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import { ThemeProvider } from "./ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 export default Provider;
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,25 @@
-function UserIcon() {
-  return <div>UserIcon</div>;
+import { currentUser } from "@clerk/nextjs/server";
+import { LuUser2 } from "react-icons/lu";
+
+async function UserIcon() {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img
+        alt="profile image"
+        src={profileImage}
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  }
+
+  return (
+    <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white">
+      UserIcon
+    </LuUser2>
+  );
 }
 export default UserIcon;
